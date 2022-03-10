ad85b075ce8b497ebea8264cc7c2115acb192701
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2022-03-10 11:44:23 +0700

    Taufiq-speed up, max speed = 10

--- WGS PROJECT/Assets/Package Minigame/WGS/Script/WGS_PlayerRun.cs
+++ WGS PROJECT/Assets/Package Minigame/WGS/Script/WGS_PlayerRun.cs
@@ -34,6 +34,10 @@ public class WGS_PlayerRun : MonoBehaviour
             TargetAnimator.Play(AnimIdle);
         }
 
+        if (PlayerSpeed >= 10 ){ // speed max = 10
+            PlayerSpeed = 10;
+        }
+
         Player.transform.position += new Vector3(0, 0, PlayerSpeed * Time.deltaTime);
     }
 }
