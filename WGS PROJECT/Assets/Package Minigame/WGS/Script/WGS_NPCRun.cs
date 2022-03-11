238508e42cc3b5a51656abd274415fa4c62c72e1
roedavan <30891563+roedavan@users.noreply.github.com>
2022-03-11 09:32:59 +0700

    nambah script lari NPC
    nambah script lari NPC

--- WGS PROJECT/Assets/Package Minigame/WGS/Script/WGS_NPCRun.cs
+++ WGS PROJECT/Assets/Package Minigame/WGS/Script/WGS_NPCRun.cs
@@ -2,42 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WGS_PlayerRun : MonoBehaviour
+public class WGS_NPCRun : MonoBehaviour
 {
 
     public GameObject Player;
     public Animator TargetAnimator;
     public string AnimIdle;
     public string AnimRun;
-    public float PlayerSpeed;
+    public float MaxPlayerSpeed;
+    float PlayerSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        PlayerSpeed += 0.01f;
+        if (PlayerSpeed >= MaxPlayerSpeed)
         {
-            PlayerSpeed += 1f;
-            TargetAnimator.Play(AnimRun);
+            PlayerSpeed = MaxPlayerSpeed;
         }
-
-        if (PlayerSpeed >= 0)
-        {
-            PlayerSpeed -= 0.01f;
-        } else
-        {
-            TargetAnimator.Play(AnimIdle);
-        }
-
-        if (PlayerSpeed >= 10 ){ // speed max = 10
-            PlayerSpeed = 10;
-        }
-
+        TargetAnimator.Play(AnimRun);
         Player.transform.position += new Vector3(0, 0, PlayerSpeed * Time.deltaTime);
     }
 }
