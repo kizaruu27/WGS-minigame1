0b07c4875ef8ae34e8f9bd6b403f4b04f2b12267
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-03-11 14:32:48 +0700

    random prefab spawn

--- WGS PROJECT/Assets/Package Minigame/WGS/Script/SpawnItems.cs
+++ WGS PROJECT/Assets/Package Minigame/WGS/Script/SpawnItems.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnItems : MonoBehaviour
 {
-    [SerializeField] GameObject itemsPrefabs;
+    [SerializeField] GameObject[] itemsPrefabs;
     [SerializeField] float spawnTimes;
     // Start is called before the first frame update
     void Start()
@@ -13,7 +13,7 @@ public class SpawnItems : MonoBehaviour
     }
 
     IEnumerator SpawnItem() {
-        Instantiate(itemsPrefabs, transform.position, transform.rotation);
+        Instantiate(itemsPrefabs[Random.Range(0, itemsPrefabs.Length)], transform.position, transform.rotation);
         yield return new WaitForSeconds(spawnTimes);
 
         StartCoroutine(SpawnItem());
