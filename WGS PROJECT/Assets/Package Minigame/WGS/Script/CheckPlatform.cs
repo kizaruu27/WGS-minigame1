5b28cabf6d019f5e2ea486e3218d956dc6a575c6
anggaadypratama <anggaadypratama@outlook.com>
2022-03-18 04:01:17 +0700

    feature : check platform

---
+++ WGS PROJECT/Assets/Package Minigame/WGS/Script/CheckPlatform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPlatform : MonoBehaviour
+{
+    public static RuntimePlatform app = Application.platform;
+
+
+    //Platform
+    public static bool isWeb { get => app == RuntimePlatform.WebGLPlayer; }
+    public static bool isAndroid { get => app == RuntimePlatform.Android; }
+    public static bool isIos { get => app == RuntimePlatform.IPhonePlayer; }
+    public static bool isMacUnity { get => app == RuntimePlatform.OSXEditor; }
+    public static bool isWindowsUnity { get => app == RuntimePlatform.WindowsEditor; }
+
+
+    //Device
+    public static bool isDesktop { get => SystemInfo.deviceType == DeviceType.Desktop; }
+    public static bool isMobile { get => SystemInfo.deviceType == DeviceType.Handheld; }
+
+
+}
