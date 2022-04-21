0046472428fc68a1563bc1b6affa5c34ba106a88
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2022-04-21 17:38:48 +0700

    recover error NullReferenceException on NPC

--- WGS PROJECT/Assets/Package Minigame/WGS/Script/util/CheckAllPlayerConnected.cs
+++ WGS PROJECT/Assets/Package Minigame/WGS/Script/util/CheckAllPlayerConnected.cs
@@ -6,10 +6,8 @@ using System;
 
 public class CheckAllPlayerConnected : MonoBehaviour
 {
-
     public static CheckAllPlayerConnected instance;
     private void Awake() => instance = this;
-
     public IEnumerator WaitAllPlayerReady(Func<Coroutine> ActionMethod)
     {
         yield return new WaitUntil(() => GameObject.FindGameObjectsWithTag("Player").Length == (int)PhotonNetwork.PlayerList.Length);
