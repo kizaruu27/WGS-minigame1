c27814ea23da7816917aacd714caef999c733e80
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-05-18 15:48:35 +0700

    create new camera shake

--- WGS PROJECT/Assets/Package Minigame/WGS/Script/Mechanics/Camera/CameraController.cs
+++ WGS PROJECT/Assets/Package Minigame/WGS/Script/Mechanics/Camera/CameraController.cs
@@ -1,24 +1,14 @@
 using UnityEngine;
 
-
 namespace RunMinigames.Mechanics.Cam
 {
     public class CameraController : MonoBehaviour
     {
         public Transform Player;
-        public Vector3 offset;
-
-        public float smoothFactor = 0.5f;
-
-        void Start()
-        {
-            offset = transform.position - Player.position;
-        }
 
         void LateUpdate()
         {
-            Vector3 newPosition = Player.position + offset;
-            transform.position = Vector3.Slerp(transform.position, newPosition, smoothFactor);
+            transform.position = Player.position;
         }
     }
 }
