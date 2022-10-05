66328cd89a2a34e3e8addd8273ea5cf227572f96
anggaadypratama <anggaadypratama@outlook.com>
2022-10-05 12:47:02 +0700

    fix: jump spam when touch in mobile

--- WGS PROJECT/Assets/_MAIN/_Scripts/Mechanics/Characters/Input/PlayerInput.cs
+++ WGS PROJECT/Assets/_MAIN/_Scripts/Mechanics/Characters/Input/PlayerInput.cs
@@ -6,114 +6,116 @@ using RunMinigames.Mechanics.Characters;
 //! Jangan Lupa tambah Multiplayer
 namespace RunMinigames.Mechanics.Characters
 {
-    public class PlayerInput : PlayerController
+  public class PlayerInput : PlayerController
+  {
+    [Header("Mobile")]
+    [SerializeField] bool IsControlBtnActive = false;
+    float screenWidth;
+    Button btnJump;
+    Button btnRun;
+    public PhotonView view;
+
+
+    private void Awake()
     {
-        [Header("Mobile")]
-        [SerializeField] bool IsControlBtnActive = false;
-        float screenWidth;
-        Button btnJump;
-        Button btnRun;
-        public PhotonView view;
+      btnJump = FindInActiveObjectByName("BtnJump")?.GetComponent<Button>();
+      btnRun = FindInActiveObjectByName("BtnRun")?.GetComponent<Button>();
 
-
-        private void Awake()
-        {
-            btnJump = FindInActiveObjectByName("BtnJump")?.GetComponent<Button>();
-            btnRun = FindInActiveObjectByName("BtnRun")?.GetComponent<Button>();
-
-            Rb = GetComponent<Rigidbody>();
-        }
-
-        private void Start()
-        {
-            // btnJump.gameObject.SetActive((CheckPlatform.isAndroid || CheckPlatform.isIos) && IsControlBtnActive);
-            // btnRun.gameObject.SetActive((CheckPlatform.isAndroid || CheckPlatform.isIos) && IsControlBtnActive);
-
-            screenWidth = Screen.width;
-        }
-
-        GameObject FindInActiveObjectByName(string name)
-        {
-            Button[] objs = Resources.FindObjectsOfTypeAll<Button>() as Button[];
-            for (int i = 0; i < objs.Length; i++)
-            {
-                if (objs[i].hideFlags == HideFlags.None)
-                {
-                    if (objs[i].name == name)
-                    {
-                        return objs[i].gameObject;
-                    }
-                }
-            }
-            return null;
-        }
-
-        private void Update()
-        {
-
-            if (view.IsMine)
-            {
-                // Desktop();
-                Mobile();
-            }
-
-            Movement();
-            GroundCheck();
-        }
-
-        private void OnEnable()
-        {
-            //Button Control
-            // if (CheckPlatform.isIos || CheckPlatform.isAndroid && IsControlBtnActive && view.IsMine)
-            // {
-            //     btnJump.onClick.AddListener(Jump);
-            //     btnRun.onClick.AddListener(() => Running());
-            // }
-        }
-
-        void Desktop()
-        {
-            if (
-                CheckPlatform.isMacUnity ||
-                CheckPlatform.isWindowsUnity ||
-                CheckPlatform.isWeb ||
-                CheckPlatform.isMac ||
-                CheckPlatform.isWindows
-            )
-            {
-                if (UnityEngine.Input.GetKeyDown(KeyCode.Mouse0))
-                {
-                    Running();
-                }
-
-                if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
-                {
-                    Jump();
-                }
-            }
-        }
-
-        void Mobile()
-        {
-            int i = 0;
-
-            while (i < UnityEngine.Input.touchCount)
-            {
-                if (UnityEngine.Input.GetTouch(i).position.x > screenWidth / 2)
-                {
-                    if (canMove && !isItemSpeedActive)
-                    {
-                        Running(.07f);
-                    }
-                }
-
-                if (UnityEngine.Input.GetTouch(i).position.x < screenWidth / 2)
-                {
-                    Jump();
-                }
-
-                ++i;
-            }
-        }
+      Rb = GetComponent<Rigidbody>();
     }
+
+    private void Start()
+    {
+      // btnJump.gameObject.SetActive((CheckPlatform.isAndroid || CheckPlatform.isIos) && IsControlBtnActive);
+      // btnRun.gameObject.SetActive((CheckPlatform.isAndroid || CheckPlatform.isIos) && IsControlBtnActive);
+
+      screenWidth = Screen.width;
+    }
+
+    GameObject FindInActiveObjectByName(string name)
+    {
+      Button[] objs = Resources.FindObjectsOfTypeAll<Button>() as Button[];
+      for (int i = 0; i < objs.Length; i++)
+      {
+        if (objs[i].hideFlags == HideFlags.None)
+        {
+          if (objs[i].name == name)
+          {
+            return objs[i].gameObject;
+          }
+        }
+      }
+      return null;
+    }
+
+    private void Update()
+    {
+
+      if (view.IsMine)
+      {
+        // Desktop();
+        Mobile();
+      }
+
+      Movement();
+      GroundCheck();
+    }
+
+    private void OnEnable()
+    {
+      //Button Control
+      // if (CheckPlatform.isIos || CheckPlatform.isAndroid && IsControlBtnActive && view.IsMine)
+      // {
+      //     btnJump.onClick.AddListener(Jump);
+      //     btnRun.onClick.AddListener(() => Running());
+      // }
+    }
+
+    void Desktop()
+    {
+      if (
+          CheckPlatform.isMacUnity ||
+          CheckPlatform.isWindowsUnity ||
+          CheckPlatform.isWeb ||
+          CheckPlatform.isMac ||
+          CheckPlatform.isWindows
+      )
+      {
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Mouse0))
+        {
+          Running();
+        }
+
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+        {
+          Jump();
+        }
+      }
+    }
+
+    void Mobile()
+    {
+      int i = 0;
+
+      while (i < UnityEngine.Input.touchCount)
+      {
+        bool isTap = Input.GetTouch(i).phase == TouchPhase.Began;
+
+        if (UnityEngine.Input.GetTouch(i).position.x > screenWidth / 2)
+        {
+          if (canMove && !isItemSpeedActive)
+          {
+            Running(.07f);
+          }
+        }
+
+        if (UnityEngine.Input.GetTouch(i).position.x < screenWidth / 2 && isTap)
+        {
+          Jump();
+        }
+
+        ++i;
+      }
+    }
+  }
 }
